85f0c086002617160011251711e53dc5d7b7927a
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2024-02-13 17:10:13 +0530

    done with the task

--- Task 8 Pagination/script.js
+++ Task 8 Pagination/script.js
@@ -2,7 +2,12 @@
 let table = document.querySelector('tbody');
 let detailsCont = document.querySelector('.Details-container');
 
+// window.onload = function(){
+            
+
+
 var z = btn.addEventListener('click', function () {
+    // function fetchInitialize(){
     fetch('https://dummyjson.com/users')
         .then((d1) => {
             return d1.json();
@@ -10,21 +15,43 @@ var z = btn.addEventListener('click', function () {
         .then((res) => {
             console.log(res.users)
             let users = res.users;
-            let user = users.map((item, index) => {
-                var row = ` <tr onclick="showPopup(${item.id})">    
-                            <td>${item.id}</td>
-                            <td>${item.firstName}</td>
-                            <td>${item.lastName}</td>
-                            <td>${item.age}</td>
-                            </tr>`
-                return row
-            })
-            return user
+            // let user = users.map((item, index) => {
+            //     var row = `<tr onclick="showPopup(${item.id})">    
+            //                 <td>${item.id}</td>
+            //                 <td>${item.firstName}</td>
+            //                 <td>${item.lastName}</td>
+            //                 <td>${item.age}</td>
+            //                 </tr>`
+
+            //     return row
+            // })
+            // return user  
+           
+            const pagTable = $('#employeeTable').DataTable({
+                data: users,
+                columns: [
+                    { data: 'id' },
+                    { data: 'firstName' },
+                    { data: 'lastName' },
+                    { data: 'age' },
+                ],
+                paging: true, // Enable pagination
+                lengthMenu: [5, 10,25], // Define page length options
+                pageLength: 10, // Set default page length
+                // displayStart: 0 // display the first page
+            });
+            table.page('first').draw('page');
+            return pagTable;
         })
-        .then((res1) => {
+            
+        .then((res1) => {         
             table.innerHTML = res1.join('');
         })
+    // }
+    // fetchInitialize();
+
 })
+// }
 
 function showPopup(id) {
     fetch(`https://dummyjson.com/user/${id}`)
@@ -58,129 +85,152 @@ function closePopUp() {
 // -------------------------------------------------------------------
 // PAGINATION
 
-let pgbtn1 = document.getElementById('btnNum1');
-let pgbtn2 = document.getElementById('btnNum2');
-let pgbtn3 = document.getElementById('btnNum3');
+// let pgbtn1 = document.getElementById('btnNum1');
+// let pgbtn2 = document.getElementById('btnNum2');
+// let pgbtn3 = document.getElementById('btnNum3');
 
-pgbtn1.addEventListener('click', function () {
-    btnCol1();
-    fetch('https://dummyjson.com/users')
-        .then((d3) => {
-            return d3.json();
-        })
-        .then((res1) => {
-            console.log(res1.users)
-            let users = res1.users;
+// pgbtn1.addEventListener('click', function () {
+//     btnCol1();
+//     fetch('https://dummyjson.com/users')
+//         .then((d3) => {
+//             return d3.json();
+//         })
+//         .then((res1) => {
+//             console.log(res1.users)
+//             let users = res1.users;
 
-            let arr = [];
-            for (let i = 0; i < 10; i++) {
-                console.log(users[i]);
-                var row = ` <tr onclick="showPopup(${users[i].id})">    
-                        <td>${users[i].id}</td>
-                        <td>${users[i].firstName}</td>
-                        <td>${users[i].lastName}</td>
-                        <td>${users[i].age}</td>
-                        </tr>`
-                arr.push(row);
-            }
-            console.log(arr);
-            return arr;
-        })
-        .then((res2) => {
-            table.innerHTML = res2.join('');
-        })
-})
-
-
-pgbtn2.addEventListener('click', function () {
-    btnCol2();
-    fetch('https://dummyjson.com/users')
-        .then((d3) => {
-            return d3.json();
-        })
-        .then((res1) => {
-            console.log(res1.users)
-            let users = res1.users;
-
-            let arr = []
-            for (let i = 10; i < 20; i++) {
-                console.log(users[i]);
-                var row = ` <tr onclick="showPopup(${users[i].id})">    
-                        <td>${users[i].id}</td>
-                        <td>${users[i].firstName}</td>
-                        <td>${users[i].lastName}</td>
-                        <td>${users[i].age}</td>
-                        </tr>`
-                arr.push(row);
-            }
-            console.log(arr);
-            return arr;
-        })
-        .then((res2) => {
-            table.innerHTML = res2.join('');
-        })
-})
+//             let arr = [];
+//             for (let i = 0; i < 10; i++) {
+//                 console.log(users[i]);
+//                 var row = ` <tr onclick="showPopup(${users[i].id})">    
+//                         <td>${users[i].id}</td>
+//                         <td>${users[i].firstName}</td>
+//                         <td>${users[i].lastName}</td>
+//                         <td>${users[i].age}</td>
+//                         </tr>`
+//                 arr.push(row);
+//             }
+//             console.log(arr);
+//             return arr;
+//         })
+//         .then((res2) => {
+//             table.innerHTML = res2.join('');
+//         })
+// })
 
 
-pgbtn3.addEventListener('click', function () {
-    btnCol3(); 
-    fetch('https://dummyjson.com/users')
-        .then((d3) => {
-            return d3.json();
-        })
-        .then((res1) => {
-            console.log(res1.users)
-            let users = res1.users;
+// pgbtn2.addEventListener('click', function () {
+//     btnCol2();
+//     fetch('https://dummyjson.com/users')
+//         .then((d3) => {
+//             return d3.json();
+//         })
+//         .then((res1) => {
+//             console.log(res1.users)
+//             let users = res1.users;
 
-            let arr = []
-            for (let i = 20; i < 30; i++) {
-                console.log(users[i]);
-                var row = ` <tr onclick="showPopup(${users[i].id})">    
-                        <td>${users[i].id}</td>
-                        <td>${users[i].firstName}</td>
-                        <td>${users[i].lastName}</td>
-                        <td>${users[i].age}</td>
-                        </tr>`
-                arr.push(row);
-            }
-            console.log(arr);
-            return arr;
-        })
-        .then((res2) => {
-            table.innerHTML = res2.join('');
-        })
-})
+//             let arr = []
+//             for (let i = 10; i < 20; i++) {
+//                 console.log(users[i]);
+//                 var row = ` <tr onclick="showPopup(${users[i].id})">    
+//                         <td>${users[i].id}</td>
+//                         <td>${users[i].firstName}</td>
+//                         <td>${users[i].lastName}</td>
+//                         <td>${users[i].age}</td>
+//                         </tr>`
+//                 arr.push(row);
+//             }
+//             console.log(arr);
+//             return arr;
+//         })
+//         .then((res2) => {
+//             table.innerHTML = res2.join('');
+//         })
+// })
 
 
-function btnCol1() {
-    if (pgbtn1) {
-        pgbtn1.style.backgroundColor = "blue";
-        pgbtn1.style.color = "white";
-        pgbtn2.style.backgroundColor = "";
-        pgbtn2.style.color = "";
-        pgbtn3.style.backgroundColor = "";
-        pgbtn3.style.color = "";
-    }
-}
-function btnCol2() {
-    if (pgbtn2) {
-        pgbtn1.style.backgroundColor = "";
-        pgbtn1.style.color = "";
-        pgbtn3.style.backgroundColor = "";
-        pgbtn3.style.color = "";
-        pgbtn2.style.backgroundColor = "blue";
-        pgbtn2.style.color = "white";
-    }
-}
-function btnCol3() {
-    if (pgbtn3) {
-        pgbtn1.style.backgroundColor = "";
-        pgbtn1.style.color = "";
-        pgbtn2.style.backgroundColor = "";
-        pgbtn2.style.color = "";
-        pgbtn3.style.backgroundColor = "blue";
-        pgbtn3.style.color = "white";
-    }
-}
+// pgbtn3.addEventListener('click', function () {
+//     btnCol3(); 
+//     fetch('https://dummyjson.com/users')
+//         .then((d3) => {
+//             return d3.json();
+//         })
+//         .then((res1) => {
+//             console.log(res1.users)
+//             let users = res1.users;
+
+//             let arr = []
+//             for (let i = 20; i < 30; i++) {
+//                 console.log(users[i]);
+//                 var row = ` <tr onclick="showPopup(${users[i].id})">    
+//                         <td>${users[i].id}</td>
+//                         <td>${users[i].firstName}</td>
+//                         <td>${users[i].lastName}</td>
+//                         <td>${users[i].age}</td>
+//                         </tr>`
+//                 arr.push(row);
+//             }
+//             console.log(arr);
+//             return arr;
+//         })
+//         .then((res2) => {
+//             table.innerHTML = res2.join('');
+//         })
+// })
+
+
+// function btnCol1() {
+//     if (pgbtn1) {
+//         pgbtn1.style.backgroundColor = "blue";
+//         pgbtn1.style.color = "white";
+//         pgbtn2.style.backgroundColor = "";
+//         pgbtn2.style.color = "";
+//         pgbtn3.style.backgroundColor = "";
+//         pgbtn3.style.color = "";
+//     }
+// }
+// function btnCol2() {
+//     if (pgbtn2) {
+//         pgbtn1.style.backgroundColor = "";
+//         pgbtn1.style.color = "";
+//         pgbtn3.style.backgroundColor = "";
+//         pgbtn3.style.color = "";
+//         pgbtn2.style.backgroundColor = "blue";
+//         pgbtn2.style.color = "white";
+//     }
+// }
+// function btnCol3() {
+//     if (pgbtn3) {
+//         pgbtn1.style.backgroundColor = "";
+//         pgbtn1.style.color = "";
+//         pgbtn2.style.backgroundColor = "";
+//         pgbtn2.style.color = "";
+//         pgbtn3.style.backgroundColor = "blue";
+//         pgbtn3.style.color = "white";
+//     }
+// }
+
+
+// -------------------------------------------------------------------
+
+// Fetch employee data using Fetch API
+// fetch('https://api.example.com/employees')
+//   .then(response => response.json())
+//   .then(data => {
+//     // Populate table rows with employee data
+//     const table = $('#employeeTable').DataTable({
+//       data: data,
+//       columns: [
+//         { data: 'name' },
+//         { data: 'age' },
+//         // Define more columns as needed
+//       ],
+//       paging: true, // Enable pagination
+//       lengthMenu: [5, 10, 25, 50], // Define page length options
+//       pageLength: 10 // Set default page length
+//     });
+//   });
+
+
 
 
